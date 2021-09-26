10dd0752836cf7918f4c568439a6b988f9410b29
Ahti Ahde <ahti.ahde@gmail.com>
2021-09-26 19:47:19 +0300

    Add exit code to release script

--- admin_tools/download-100.sh
+++ admin_tools/download-100.sh
@@ -4,3 +4,4 @@ FILECOUNT="$(ls -la static/books/ | wc -l)"
 if [ "$FILECOUNT" -lt 5 ]; then
     wget -O - https://www.gutenberg.org/browse/scores/top | sed -n 's/.*href="\/ebooks\/\([^"]*\).*/\1/p' | head -100 | tail -96 | awk '{print "https://www.gutenberg.org/files/"$1"/"$1"-0.txt"}' | wget -w 1 -P static/books/ -i -
 fi
+exit 0
