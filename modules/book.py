edc2d11ce01af594db43e571e9d7980e14ca2400
Ahti Ahde <ahti.ahde@gmail.com>
2022-01-05 09:53:40 +0200

    Fix insert / update problem

--- modules/book.py
+++ modules/book.py
@@ -1,7 +1,24 @@
 class Book():
     def __init__(self, db):
         self.db = db
-        
+
+    ### General Helpers ###
+    
+    def _get_role_annotations(self, book_id, user_id):
+        return self.db.session.execute('''
+                SELECT annotool.characters.name, COALESCE(annotool.role_annotations.role, 'Unknown') AS role 
+                    FROM annotool.characters 
+                LEFT JOIN annotool.role_annotations 
+                    ON (annotool.characters.id=annotool.role_annotations.char_id 
+                        AND annotool.role_annotations.user_id=:user_id)
+                WHERE 
+                    annotool.characters.book_id=:book_id''',
+                {
+                    "book_id": book_id, 
+                    "user_id": user_id
+                })
+    
+    ## This method has been "overloaded" for handling the writes too
     def parse_characters(self, book_id, user_id=-1):
         book = {}
         result = self.db.session.execute(
@@ -15,18 +32,7 @@ class Book():
                 "SELECT * FROM annotool.characters WHERE book_id=:book_id",
                 {"book_id": book_id})
         else:
-            result = self.db.session.execute('''
-                SELECT annotool.characters.name, COALESCE(annotool.role_annotations.role, 'Unknown') AS role 
-                    FROM annotool.characters 
-                LEFT JOIN annotool.role_annotations 
-                    ON (annotool.characters.id=annotool.role_annotations.char_id 
-                        AND annotool.role_annotations.user_id=:user_id)
-                WHERE 
-                    annotool.characters.book_id=:book_id''',
-                {
-                    "book_id": book_id, 
-                    "user_id": user_id
-                })
+            result = self._get_role_annotations(book_id, user_id)
     
         book['characters'] = result.fetchall()
         return book
@@ -55,21 +61,44 @@ class Book():
     
     def write_role_annotations(self, book_id, user_id, form_annotations):
         book = self.parse_characters(book_id)
+        query = self.db.session.execute(
+            "SELECT char_id FROM annotool.role_annotations WHERE user_id=:user_id",
+            {"user_id": user_id}).fetchall()
+        
+        existing_annotations = []
+        print(query)
+        for x in query:
+            existing_annotations.append(x[0])
         
         char_name_to_id = {}
         for character in book['characters']:
             char_name_to_id[character.name] = character.id
 
-        annotations = []
+        inserts = []
+        updates = []
         for key, value in form_annotations:
             if "role-" in key:
-                annotations.append({
-                    "user_id": user_id,
-                    "char_id": char_name_to_id[key.split('-')[1]],
-                    "role": value
-                })
-
-        # Implement insert or update!
-        sql = "INSERT INTO annotool.role_annotations (user_id, char_id, role) VALUES (:user_id, :char_id, :role)"
-        self.db.session.execute(sql, annotations)
-        self.db.session.commit()
+                char_id = char_name_to_id[key.split('-')[1]]
+                if char_id in existing_annotations:
+                    updates.append({
+                        "user_id": user_id,
+                        "char_id": char_id,
+                        "role": value
+                    })
+                else:
+                    inserts.append({
+                        "user_id": user_id,
+                        "char_id": char_id,
+                        "role": value
+                    })
+        print(existing_annotations, inserts, updates)
+        
+        if len(inserts) > 0:
+            sql = "INSERT INTO annotool.role_annotations (user_id, char_id, role) VALUES (:user_id, :char_id, :role)"
+            self.db.session.execute(sql, inserts)
+            self.db.session.commit()
+        
+        if len(updates) > 0:
+            sql = "UPDATE annotool.role_annotations SET user_id=:user_id, role=:role WHERE char_id=:char_id"
+            self.db.session.execute(sql, updates)
+            self.db.session.commit()
