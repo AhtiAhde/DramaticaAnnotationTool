7e296d2517b1ba2f55e56e70a5ec008c5f8c9a1e
Ahti Ahde <ahti.ahde@gmail.com>
2022-01-17 09:23:30 +0200

    Refactor a bit

--- modules/arc.py
+++ modules/arc.py
@@ -36,14 +36,6 @@ class Arc():
         
         if len(ids) == 0:
             return {}
-        mice = Mice(self.db)
-        mices = mice.read_from_arc_list(ids)
-        
-        ppp = PPP(self.db)
-        ppps = ppp.read_from_arc_list(ids)
-        
-        dramatica = Dramatica(self.db)
-        dramaticas = dramatica.read_pp_from_arc_list(ids)
         
         # parse the final constructs
         ret = []
@@ -54,32 +46,61 @@ class Arc():
                 "short_desc": arc.short_desc
             }
             
-            for mice_instance in mices:
-                if mice_instance.arc_id == arc.id:
-                    arc_view['mice_type'] = mice_instance.mice_type
-                    if mice_instance.is_start_event:
-                        arc_view['mice_start'] = mice_instance.annotation_note
-                    else:
-                        arc_view['mice_end'] = mice_instance.annotation_note
-            
-            progress_list = []
-            for ppp_instance in ppps:
-                if ppp_instance.arc_id == arc.id:
-                    if ppp_instance.phase == "promise":
-                        arc_view['promise'] = ppp_instance.annotation_note
-                    if ppp_instance.phase == "payoff":
-                        arc_view['payoff'] = ppp_instance.annotation_note
-                    if ppp_instance.phase == "progress":
-                        progress_list.append(ppp_instance)
-            arc_view['progresses'] = progress_list
-            
-            for dramatica_instance in dramaticas:
-                if dramatica_instance.arc_id == arc.id:
-                    arc_view['dramatica_pp'] = dramatica_instance.plot_point
-                    arc_view['dramatica_pp_theme'] = dramatica_instance.theme
-                    arc_view['dramatica_pp_note'] = dramatica_instance.annotation_note
+            arc_view['mice_type'], arc_view['mice_start'], arc_view['mice_end'] = self._read_mice(ids, arc.id)
+            arc_view['promise'], arc_view['payoff'], arc_view['progresses'] = self._read_ppp(ids, arc.id)
+            arc_view['dramatica_pp'], arc_view['dramatica_pp_theme'], arc_view['dramatica_pp_note'] = self._read_dramatica(ids, arc.id)
+
             ret.append(arc_view)
         return ret
+
+    def _read_mice(self, ids, arc_id):
+        mice = Mice(self.db)
+        mices = mice.read_from_arc_list(ids)
+        mice_type = ""
+        mice_start = ""
+        mice_end = ""
+        
+        for mice_instance in mices:
+            if mice_instance.arc_id == arc_id:
+                 mice_type = mice_instance.mice_type
+            if mice_instance.is_start_event:
+                mice_start = mice_instance.annotation_note
+            else:
+                mice_end = mice_instance.annotation_note
+        
+        return mice_type, mice_start, mice_end
+
+    def _read_ppp(self, ids, arc_id):
+        ppp = PPP(self.db)
+        ppps = ppp.read_from_arc_list(ids)
+        
+        promise = ""
+        payoff = ""
+        progress_list = []
+        for ppp_instance in ppps:
+            if ppp_instance.arc_id == arc_id:
+                if ppp_instance.phase == "promise":
+                    promise = ppp_instance.annotation_note
+                if ppp_instance.phase == "payoff":
+                    payoff = ppp_instance.annotation_note
+                if ppp_instance.phase == "progress":
+                    progress_list.append(ppp_instance)
+        
+        return promise, payoff, progress_list
+    
+    def _read_dramatica(self, ids, arc_id):
+        dramatica = Dramatica(self.db)
+        dramaticas = dramatica.read_pp_from_arc_list(ids)
+        
+        plot_point = ""
+        theme = ""
+        note = ""
+        for dramatica_instance in dramaticas:
+            if dramatica_instance.arc_id == arc_id:
+                plot_point = dramatica_instance.plot_point
+                theme = dramatica_instance.theme
+                note = dramatica_instance.annotation_note
+        return plot_point, theme, note       
 
     def update_annotations(self, book_id, user_id, arc_id, form_data):
         # check authorization
@@ -96,7 +117,16 @@ class Arc():
         if len(result) != 1:
             print("Unoauthorized user", user_id, book_id, arc_id)
         
-        # mice annotations
+        self._update_mice_annotations(arc_id, form_data)
+        self._update_ppp_annotations(arc_id, form_data)
+        self._update_dramatica_annotations(arc_id, form_data)        
+             
+        # All the commands are run with commit=False flag, so that they
+        # can be run us single coherent transaction
+        self.db.session.commit()
+    
+    # This method does not commit the changes to database
+    def _update_mice_annotations(self, arc_id, form_data):
         mice = Mice(self.db)
         start_mice = None
         end_mice = None
@@ -118,7 +148,6 @@ class Arc():
             if mice_type != start_mice['mice_type']:
                 start_mice['mice_type'] = mice_type
                 end_mice['mice_type'] = mice_type
-                # TODO: try if these row objects could be directly used for updating
                 mice.update_mice_type(start_mice, False)
                 mice.update_mice_type(end_mice, False)
             if start_mice['annotation_note'] != start_note:
@@ -131,7 +160,8 @@ class Arc():
             mice.create_from_arc(arc_id, mice_type, start_note, True, False)
             mice.create_from_arc(arc_id, mice_type, end_note, False, False)
         
-        # handle Promises Progresses and Payoffs
+    # This method does not commit the changes to database
+    def _update_ppp_annotations(self, arc_id, form_data):
         ppp = PPP(self.db)
         promise = None
         payoff = None
@@ -161,25 +191,22 @@ class Arc():
             elif not payoff:
                 ppp.create_from_arc(arc_id, 'payoff', form_data['payoff'], False)
         
-        
         if form_data['new-progress'] != "":
             ppp.create_from_arc(arc_id, 'progress', form_data['new-progress'], False)
-        
-        # handle Dramatica
+     
+    # This method does not commit the changes to database
+    def _update_dramatica_annotations(self, arc_id, form_data):
         dramatica = Dramatica(self.db)
-        promise = None
-        payoff = None
-        progresses = []
         
         existing_dramatica_pp = dramatica.read_pp_from_arc(arc_id)
         
-        if len(existing_annotation) == 1:
-            existing_annotation = dict(existing_annotation[0])
+        if len(existing_dramatica_pp) == 1:
+            existing_dramatica_pp = dict(existing_dramatica_pp[0])
             if existing_dramatica_pp['plot_point'] != form_data['dramatica-pp']:
                 existing_dramatica_pp['plot_point'] = form_data['dramatica-pp']
             if existing_dramatica_pp['theme'] != form_data['dramatica-pp-theme']:
                 existing_dramatica_pp['theme'] = form_data['dramatica-pp-theme']
-            if existing_dramatica_pp['annotaion_note'] != form_data['dramatica-pp-note']:
+            if existing_dramatica_pp['annotation_note'] != form_data['dramatica-pp-note']:
                 existing_dramatica_pp['annotation_note'] = form_data['dramatica-pp-note']
             dramatica.update_pp(existing_dramatica_pp, False)
         elif form_data['dramatica-pp'] != 'unknown' or form_data['dramatica-pp-theme'] != 'unknown':
@@ -189,9 +216,3 @@ class Arc():
                 form_data['dramatica-pp-theme'],
                 form_data['dramatica-pp-note']
             )
-            
-            
-        
-        self.db.session.commit()
-
-        
